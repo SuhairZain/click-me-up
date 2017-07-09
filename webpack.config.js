9ed57e514770a5f7d63b90d19cd4cd9e1402b546
Suhair Zain <suhair@qplaytech.com>
2017-07-09 19:30:52 +0530

    Updated page title

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,71 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const PROD = JSON.parse(process.env.PROD_ENV || '0');
+const PROD = JSON.parse(process.env.PROD_ENV || "0");
 
 const links = [
-    'https://fonts.googleapis.com/css?family=Open+Sans',
-    `${PROD ? '' : '/'}css/styles.css`
+  "https://fonts.googleapis.com/css?family=Open+Sans",
+  `${PROD ? "" : "/"}css/styles.css`
 ];
 
 const plugins = [
-    new HtmlWebpackPlugin({
-        filename: '../index.html',
-        links: links,
-        template: 'index-file-template.ejs',
-        title: "Screen Size Calculator"
-    })
+  new HtmlWebpackPlugin({
+    filename: "../index.html",
+    links: links,
+    template: "index-file-template.ejs",
+    title: "Click Me Up"
+  })
 ];
 
-const outputDir = 'dist';
+const outputDir = "dist";
 
 if (PROD) {
-    fs.readdirSync(`./${outputDir}`).forEach((filename) => {
-        fs.unlinkSync(`./${outputDir}/${filename}`);
-    });
+  fs.readdirSync(`./${outputDir}`).forEach(filename => {
+    fs.unlinkSync(`./${outputDir}/${filename}`);
+  });
 
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false
-            }
-        })
-    );
-    plugins.push(
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    );
-    plugins.push(new webpack.optimize.DedupePlugin());
-    plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  );
+  plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  );
+  plugins.push(new webpack.optimize.DedupePlugin());
+  plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 }
 
 module.exports = {
-    entry: './src/main.jsx',
-    output: {
-        path: './dist',
-        publicPath: `${PROD ? '' : '/'}dist/`,
-        filename: `bundle${PROD ? '_' + new Date().getTime() : ''}.js`
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.css$/,
-            loaders: ['style', 'css']
-        }]
-    },
-    plugins: plugins
+  entry: "./src/main.jsx",
+  output: {
+    path: "./dist",
+    publicPath: `${PROD ? "" : "/"}dist/`,
+    filename: `bundle${PROD ? "_" + new Date().getTime() : ""}.js`
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style", "css"]
+      }
+    ]
+  },
+  plugins: plugins
 };
